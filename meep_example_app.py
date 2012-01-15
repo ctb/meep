15fb9000b89ccc6d45fb763ac8d260c7d0405d9c
Chelsea Carr <carrche2@msu.edu>
2012-01-15 02:03:30 -0500

    Added Delete message functionality

--- meep_example_app.py
+++ meep_example_app.py
@@ -1,6 +1,7 @@
 import meeplib
 import traceback
 import cgi
+import Tkinter
 
 def initialize():
     # create a default user
@@ -11,6 +12,9 @@ def initialize():
 
     # done.
 
+def callback():
+    print "click!"
+
 class MeepExampleApp(object):
     """
     WSGI app object.
@@ -20,7 +24,7 @@ class MeepExampleApp(object):
 
         username = 'test'
 
-        return ["""you are logged in as user: %s.<p><a href='/m/add'>Add a message</a><p><a href='/login'>Log in</a><p><a href='/logout'>Log out</a><p><a href='/m/list'>Show messages</a>""" % (username,)]
+        return ["""you are logged in as user: %s.<p><a href='/m/add'>Add a message</a><p><a href='/login'>Log in</a><p><a href='/logout'>Log out</a><p><a href='/m/messages'>Show messages</a><p><a href='/m/delete'>Delete a message</a>""" % (username,)]
 
     def login(self, environ, start_response):
         # hard code the username for now; this should come from Web input!
@@ -62,8 +66,11 @@ class MeepExampleApp(object):
             s.append('message: %s<p>' % (m.post))
             s.append('author: %s<p>' % (m.author.username))
             s.append('<hr>')
-
+        
+        if not messages:
+            s.append("There are no messages to display.<p>")
         s.append("<a href='../../'>index</a>")
+       
             
         headers = [('Content-type', 'text/html')]
         start_response("200 OK", headers)
@@ -90,18 +97,40 @@ class MeepExampleApp(object):
         new_message = meeplib.Message(title, message, user)
 
         headers = [('Content-type', 'text/html')]
-        headers.append(('Location', '/m/list'))
+        headers.append(('Location', '/m/messages'))
         start_response("302 Found", headers)
         return ["message added"]
+
+    def delete_message(self, environ, start_response):
+        headers = [('Content-type', 'text/html')]
+        
+        start_response("200 OK", headers)
+
+        return """ <h2>Please enter the id of the message to delete</h2><form action='delete_action' method='POST'>Enter ID:<input type='text' name='message'><input type='submit'></form>"""
+
+    def delete_message_action(self, environ, start_response):
+        print environ['wsgi.input']
+        form = cgi.FieldStorage(fp=environ['wsgi.input'], environ=environ)
+
+        messageID = int(form['message'].value);
+        id = meeplib.get_message(messageID)
+        meeplib.delete_message(id)
+        
+        headers = [('Content-type', 'text/html')]
+        headers.append(('Location', '/'))
+        start_response("302 Found", headers)
+        return ["message deleted"]
     
     def __call__(self, environ, start_response):
         # store url/function matches in call_dict
         call_dict = { '/': self.index,
                       '/login': self.login,
                       '/logout': self.logout,
-                      '/m/list': self.list_messages,
+                      '/m/messages': self.list_messages,
                       '/m/add': self.add_message,
-                      '/m/add_action': self.add_message_action
+                      '/m/add_action': self.add_message_action,
+                      '/m/delete': self.delete_message,
+                      '/m/delete_action': self.delete_message_action
                       }
 
         # see if the URL is in 'call_dict'; if it is, call that function.

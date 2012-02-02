29151dca83fe3630fd5a6ff54c26c7b2e85f8dfa
Eric Dostie <dostieer@msu.edu>
2012-02-02 18:46:37 -0500

    Added unit test for index page when user has been authed (logged in)

--- tests/test_app.py
+++ tests/test_app.py
@@ -9,7 +9,7 @@ class TestApp(unittest.TestCase):
         app = meep_example_app.MeepExampleApp()
         self.app = app
 
-    def test_index(self):
+    def test_index_no_auth(self):
         environ = {}                    # make a fake dict
         environ['PATH_INFO'] = '/'
 
@@ -18,8 +18,35 @@ class TestApp(unittest.TestCase):
             assert ('Content-type', 'text/html') in headers
 
         data = self.app(environ, fake_start_response)
-        assert 'Add a message' in data[0]
-        assert 'Show messages' in data[0]
+        assert 'Log in' in data[0]
+        assert 'Create a New User' in data[0]
+
+    def test_index_with_auth(self):
+        self.app.username = 'test' # force login
+        environ = {}                    # make a fake dict
+        environ['PATH_INFO'] = '/'
+
+        def fake_start_response(status, headers):
+            assert status == '200 OK'
+            assert ('Content-type', 'text/html') in headers
+
+        data = self.app(environ, fake_start_response)
+        assert 'New thread' in data[0]
+        assert 'Show threads' in data[0]
+
+    '''def test_create_user(self):
+        environ = {}                    # make a fake dict
+        environ['PATH_INFO'] = '/create_user'
+
+        def fake_start_response(status, headers):
+            print status
+            assert status == '302 Found'
+            assert ('Content-type', 'text/html') in headers
+
+        data = self.app(environ, fake_start_response)
+        print data
+        assert 'New thread' in data[0]
+        assert 'Show threads' in data[0]'''
 
     def tearDown(self):
         pass

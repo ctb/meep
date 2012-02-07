a341caf862073a0949320bb139cf426d5ad4fa71
Nitrowonder <jonesza6@msu.edu>
2012-02-07 14:06:16 -0800

    Units tests HW4

---
+++ test_app.py
@@ -0,0 +1,61 @@
+import unittest
+import meep_example_app
+
+class TestApp(unittest.TestCase):
+    def setUp(self):
+        meep_example_app.initialize()
+        app = meep_example_app.MeepExampleApp()
+        self.app = app
+
+    def test_index(self):
+        environ = {}                    # make a fake dict
+        environ['PATH_INFO'] = '/'
+
+        def fake_start_response(status, headers):
+            assert status == '200 OK'
+            assert ('Content-type', 'text/html') in headers
+
+        data = self.app(environ, fake_start_response)
+        assert 'Add a message' in data[0]
+        assert 'Show messages' in data[0]
+
+    def test_show_messages(self):
+        environ = {}                    # make a fake dict
+        environ['PATH_INFO'] = '/m/list'
+
+        def fake_start_response(status, headers):
+            assert status == '200 OK'
+            assert ('Content-type', 'text/html') in headers
+
+        data = self.app(environ, fake_start_response)
+        assert 'my title' in data[0]
+        assert 'This is my message!' in data[0]
+        
+
+    def test_delete_message_action(self):
+        environ = {}                    # make a fake dict
+        environ['PATH_INFO'] = '/m/list'
+
+        def fake_start_response(status, headers):
+            assert status == '200 OK'
+            assert ('Content-type', 'text/html') in headers
+
+        data = self.app(environ, fake_start_response)
+        assert 'delete message 0' in data[0]
+
+    def test_reply_message_action(self):
+        environ = {}                    # make a fake dict
+        environ['PATH_INFO'] = '/m/list'
+
+        def fake_start_response(status, headers):
+            assert status == '200 OK'
+            assert ('Content-type', 'text/html') in headers
+
+        data = self.app(environ, fake_start_response)
+        assert 'Reply' in data[0]
+
+    def tearDown(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()

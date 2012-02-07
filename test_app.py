108d4adea38f6d2d7f0ce068659bf02765bd8302
Jeremy Iamurri <jeremy@solatrus.com>
2012-02-07 14:09:16 -0500

    Added unit tests

--- test_app.py
+++ test_app.py
@@ -16,8 +16,42 @@ class TestApp(unittest.TestCase):
             assert ('Content-type', 'text/html') in headers
 
         data = self.app(environ, fake_start_response)
-        assert 'Add a message' in data[0]
-        assert 'Show messages' in data[0]
+        assert 'Add a topic' in data[0]
+        assert 'Show topics' in data[0]
+        
+    def test_view_topic(self):
+        environ = {}
+        environ['PATH_INFO'] = '/m/topics/view'
+        environ['QUERY_STRING'] = 'id=0'
+        
+        def fake_start_response(status, headers):
+            assert status == '200 OK'
+            assert ('Content-type', 'text/html') in headers
+            
+        data = self.app(environ, fake_start_response)
+        assert 'First Topic' in data[0]
+        
+    def test_list_topics(self):
+        environ = {}
+        environ['PATH_INFO'] = '/m/list_topics'
+        
+        def fake_start_response(status, headers):
+            assert status == '200 OK'
+            assert ('Content-type', 'text/html') in headers
+            
+        data = self.app(environ, fake_start_response)
+        assert 'First Topic' in data[0]
+        
+    def test_add_topic(self):
+        environ = {}
+        environ['PATH_INFO'] = '/m/add_topic'
+        
+        def fake_start_response(status, headers):
+            assert status == '200 OK'
+            assert ('Content-type', 'text/html') in headers
+            
+        data = self.app(environ, fake_start_response)
+        assert 'Add a new topic' in data
 
     def tearDown(self):
         pass

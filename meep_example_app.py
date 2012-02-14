c34b14d2893c1987b70c59b85c9173457a2d0b4d
Tim Belcher <belchert.msu@gmail.com>
2012-02-14 00:41:43 -0500

    Adding cookie support

--- meep_example_app.py
+++ meep_example_app.py
@@ -2,43 +2,57 @@ import meeplib
 import traceback
 import cgi
 import meepcookie
+from Cookie import SimpleCookie, Morsel
 
 def initialize():
-    #TODO
-    # Load data using Pickler here
-    
     try:    
         meeplib._load_data()
     except IOError:  # file does not exist/cannot be opened
         # initialize data from scratch
-        
         # create a default user with username: test, password: test
         u = meeplib.User('test', 'test')
 
         # create a single message and topic
         meeplib.Topic('First Topic', meeplib.Message('my title', 'This is my message!', u), u)
 
-        # done.
-
 class MeepExampleApp(object):
     """
     WSGI app object.
     """
     def index(self, environ, start_response):
-        start_response("200 OK", [('Content-type', 'text/html')])
+        #start_response("200 OK", [('Content-type', 'text/html')])
+        username = ''
+        # If a cookie exists, get it
+        try:
+            cookie_str = environ.get('HTTP_COOKIE', '')
+            cookie = SimpleCookie(cookie_str)
+            username = cookie["username"].value
+            print "Login: Username = %s" % username
+        except:
+            print "session cookie not set! defaulting username"
+            username = ''
+        
+        #If the cookie was found, redirect to main page, and set current user
+        if username is not '':
+            meeplib.set_curr_user(username)
+            headers = [('Content-type', 'text/html')]
+            headers.append(('Location','/main_page'))
+            start_response("302 Found", headers)
+            return "Cookie found, redirecting"
+        #If the cookie wasn't found, prompt the user to login
+        else:
+            start_response("200 OK", [('Content-type', 'text/html')])
+            return ["""
+                Login or <a href='/create_user'>Create an Account</a>
+                </br><p><form action='login' method='POST'>
+                Username: <input type='text' name='username'><br>
+                Password:<input type='password' name='password'><br>
+                <input type='submit' value='Login'></p></form>
+                    """]
 
-        username = 'Not Logged in'
-
-        #return ["""You are logged in as: %s.<p><a href='/m/add'>Add a message</a><p><a href='/login'>Log in</a><p><a href='/logout'>Log out</a><p><a href='/m/list'>Show messages</a>""" % (username,)]
-        #return ["""You are logged in as: %s.<p><a href='/m/add_topic'>Add a topic</a><p><a href='/login'>Log in</a><p><a href='/logout'>Log out</a><p><a href='/m/list_topics'>Show topics</a>""" % (username,)]
-        return ["""
-            Login or <a href='/create_user'>Create an Account</a>
-            </br><p><form action='login' method='POST'>
-            Username: <input type='text' name='username'><br>
-            Password:<input type='password' name='password'><br>
-            <input type='submit' value='Login'></p></form>
-        """]
-
+    ###
+    #   MAIN PAGE
+    ###
     def main_page(self, environ, start_response):
         headers = [('Content-type', 'text/html')]
         try:
@@ -53,6 +67,9 @@ class MeepExampleApp(object):
 
         return ["""You have successfully logged in as: %s<p><a href='/m/add_topic'>Add a topic</a><p><p><a href='/m/list_topics'>Show topics</a><p><a href='/logout'>Log out</a>""" % (username,)]
 
+    ###
+    #   CREATE USER
+    ###
     def create_user(self, environ, start_response):
         headers = [('Content-type', 'text/html')]
         
@@ -81,12 +98,18 @@ class MeepExampleApp(object):
 
         return [returnStatement]
 
+    ###
+    #   LOGIN
+    ###
     def login(self, environ, start_response):
         print environ['wsgi.input']
         form = cgi.FieldStorage(fp=environ['wsgi.input'], environ=environ)
 
         username = form['username'].value
         password = form['password'].value
+
+        # set content-type
+        headers = [('Content-type', 'text/html')]
 
         # Test whether variable is defined to be None
         if username is not None:
@@ -100,22 +123,36 @@ class MeepExampleApp(object):
                      meeplib.set_curr_user(username)
                      k = 'Location'
                      v = '/main_page'
+                     # Create and set the cookie
+                     cookie_name, cookie_val = \
+                                meepcookie.make_set_cookie_header('username', username)
+                     headers.append((cookie_name, cookie_val))
              else:      
                  returnStatement = """password none"""
         else:
             returnStatement = """username none"""
-
-        # set content-type
-        headers = [('Content-type', 'text/html')]
 
         headers.append((k, v))
         start_response('302 Found', headers)
         
         return """Invalid Password. Please try again."""     
 
+    ###
+    #   LOGOUT
+    ###
     def logout(self, environ, start_response):
-        # does nothing
         headers = [('Content-type', 'text/html')]
+        #Clear the cookie
+        try:
+            cookie_str = environ.get('HTTP_COOKIE', '')
+            cookie = SimpleCookie(cookie_str)
+            #I was trying to be fancy here, but couldn't get it to work
+            #cookie['username']['expires'] = meepcookie.cookie_expiration_date(-1)
+            cookie_name, cookie_val = \
+                                meepcookie.make_set_cookie_header('username', '')
+            headers.append((cookie_name, cookie_val))
+        except:
+            print "No cookie exists (this probably shouldn't happen)"
 
         # send back a redirect to '/'
         k = 'Location'
@@ -123,7 +160,7 @@ class MeepExampleApp(object):
         headers.append((k, v))
         start_response('302 Found', headers)
         
-        return "no such content"
+        return "Logged out"
 
     def list_topics(self, environ, start_response):
         topics = meeplib.get_all_topics()
@@ -132,7 +169,6 @@ class MeepExampleApp(object):
         for t in topics:
             s.append("<a href='/m/topics/view?id=%d'>%s</a>" % (int(t.id), t.title))
             s.append('<hr>')
-            
         s.append("<a href='../../main_page'>index</a>")
         
         headers = [('Content-type', 'text/html')]
@@ -140,6 +176,9 @@ class MeepExampleApp(object):
         
         return ["".join(s)]
         
+    ###
+    #   VIEW TOPIC
+    ###
     def view_topic(self, environ, start_response):
         qString = cgi.parse_qs(environ['QUERY_STRING'])
         tId = qString.get('id', [''])[0]
@@ -155,11 +194,8 @@ class MeepExampleApp(object):
             s.append('author: %s<p>' % (m.author.username))
             s.append("<form action='../delete_action' method='POST'><input type='number' hidden='true' name='mid' value=%d><input type='number' hidden='true' name='tid' value=%d><input type='text' hidden='true' name='twill' value='twill'><input type='submit' value='Delete message'></form>" % (m.id, topic.id))
             s.append('<hr>')
-
         s.append("<form action='../add_message_topic_action' method='POST'>Title: <input type='text' name='title'><br>Message:<input type='text' name='message'><br><input type='number' hidden='true' name='topicid' value=%d><input type='submit'></form>" % (topic.id))
-        
         s.append("<br><form action='../delete_topic_action' method='POST'><input type='number' hidden='true' name='tid' value=%d><input type='text' hidden='true' name='twill' value='twill'><input type='submit' value='Delete topic'></form>" % (topic.id))
-        
         s.append("<a href='../../main_page'>index</a>")
             
         headers = [('Content-type', 'text/html')]
@@ -167,6 +203,9 @@ class MeepExampleApp(object):
         
         return ["".join(s)]
     
+    ###
+    #   VIEW MESSAGES
+    ###
     def list_messages(self, environ, start_response):
         messages = meeplib.get_all_messages()
 
@@ -178,7 +217,6 @@ class MeepExampleApp(object):
             s.append('author: %s<p>' % (m.author.username))
             s.append("<form action='delete_action' method='POST'><input type='number' hidden='true' name='mid' value=%d><input type='submit' value='Delete message'></form>" % (m.id))
             s.append('<hr>')
-
         s.append("<a href='../main_page'>Back</a>")
             
         headers = [('Content-type', 'text/html')]
@@ -186,36 +224,41 @@ class MeepExampleApp(object):
         
         return ["".join(s)]
      
+    ###
+    #   ADD TOPIC
+    ###
     def add_topic(self, environ, start_response):
         headers = [('Content-type', 'text/html')]
-        
         start_response("200 OK", headers)
 
         return """<form action='add_topic_action' method='POST'>Add a new topic<br>Topic name: <input type='text' name='title'><br>Message title:<input type='text' name='msgtitle'><br>Message:<input type='text' name='message'><br><input type='submit'></form>"""
         
     def add_topic_action(self, environ, start_response):
-        print environ['wsgi.input']
         form = cgi.FieldStorage(fp=environ['wsgi.input'], environ=environ)
 
+        #Get values off form
         title = form['title'].value
         msgtitle = form['msgtitle'].value
         message = form['message'].value
         
-        username = 'test'
+        #Get the current user
+        username = meeplib.get_curr_user()
         user = meeplib.get_user(username)
         
+        #Create new message and topic
         new_message = meeplib.Message(msgtitle, message, user)
         new_topic = meeplib.Topic(title, new_message, user)
-        
 
         headers = [('Content-type', 'text/html')]
         headers.append(('Location', '/m/list_topics'))
         start_response("302 Found", headers)
         return ["topic added"]
         
+    ###
+    #   ADD MESSAGE
+    ###
     def add_message(self, environ, start_response):
         headers = [('Content-type', 'text/html')]
-        
         start_response("200 OK", headers)
 
         return """<form action='add_action' method='POST'>Title: <input type='text' name='title'><br>Message:<input type='text' name='message'><br><input type='submit'></form>"""
@@ -223,33 +266,41 @@ class MeepExampleApp(object):
     def add_message_action(self, environ, start_response):
         form = cgi.FieldStorage(fp=environ['wsgi.input'], environ=environ)
 
+        #Get values off the form
         title = form['title'].value
         message = form['message'].value
     
+        #Get current user
         username = meeplib.get_curr_user()
         user =  meeplib.get_user(username)
-        
+
+        #Create new message
         new_message = meeplib.Message(title, message, user)
 
         headers = [('Content-type', 'text/html')]
         headers.append(('Location', '/m/messages'))
         start_response("302 Found", headers)
         return ["message added"]
-		
+
+
+    ###
+    #   ADD MESSAGE TO TOPIC
+    ###
     def add_message_topic_action(self, environ, start_response):
         form = cgi.FieldStorage(fp=environ['wsgi.input'], environ=environ)
 
-        topicId = form['topicid'].value
-        topic = meeplib.get_topic(int(topicId))
-        
+        #Get values off the form
+        topicId = form['topicid'].value        
         title = form['title'].value
         message = form['message'].value
         
-        username = 'test'
+        #Get the topic and user
+        topic = meeplib.get_topic(int(topicId))
+        username = meeplib.get_curr_user()
         user = meeplib.get_user(username)
         
+        #Add the message to the topic
         new_message = meeplib.Message(title, message, user)
-        
         topic.add_message(new_message)
         
         headers = [('Content-type', 'text/html')]
@@ -257,8 +308,10 @@ class MeepExampleApp(object):
         start_response("302 Found", headers)
         return ["message added to topic"]
         
+    ###
+    #   DELETE MESSAGE
+    ###
     def delete_message_action(self, environ, start_response):
-        print environ['wsgi.input']
         form = cgi.FieldStorage(fp=environ['wsgi.input'], environ=environ)
 
         #Get the message
@@ -272,16 +325,16 @@ class MeepExampleApp(object):
         
         #Delete the message altogether
         meeplib.delete_message(message)
-        #This could all be written on one line if one so chose
-        #meeplib.delete_message(meeplib.get_message(int(form['mid'].value)))
         
         headers = [('Content-type', 'text/html')]
         headers.append(('Location', '/m/list_topics'))
         start_response("302 Found", headers)
         return ["message deleted"]
         
+    ###
+    #   DELETE TOPIC
+    ###
     def delete_topic_action(self, environ, start_response):
-        print environ['wsgi.input']
         form = cgi.FieldStorage(fp=environ['wsgi.input'], environ=environ)
 
         topicId = form['tid'].value
@@ -293,14 +346,17 @@ class MeepExampleApp(object):
         start_response("302 Found", headers)
         return ["topic deleted"]
 
+    ###
+    #   CALL HANDLER
+    ###
     def __call__(self, environ, start_response):
         # store url/function matches in call_dict
         call_dict = { '/': self.index,
                       '/main_page': self.main_page,
-                      '/create_user': self.create_user,
-                      '/create_user_action':self.create_user_action,
                       '/login': self.login,
                       '/logout': self.logout,
+                      '/create_user': self.create_user,
+                      '/create_user_action':self.create_user_action,
                       '/m/list': self.list_messages,
                       '/m/list_topics': self.list_topics,
                       '/m/topics/view': self.view_topic,
--- meepcookie.py
+++ meepcookie.py
@@ -15,3 +15,15 @@ def make_set_cookie_header(name, value, path='/'):
     s = c.output()
     (key, value) = s.split(': ')
     return (key, value)
+
+def cookie_expiration_date(numdays):
+    # FROM http://python.6.n6.nabble.com/How-to-Delete-a-Cookie-td1528033.html 
+    """
+    Returns a cookie expiration date in the required format.
+    `expires` should be a string in the format "Wdy, DD-Mon-YY HH:MM:SS GMT" 
+    NOTE!  Must use [expires] because earlier IE versions don't support [max-age].
+    A negative input value will delete the cookie 
+    """ 
+    from datetime import date, timedelta 
+    new = date.today() + timedelta(days = numdays) 
+    return new.strftime("%a, %d-%b-%Y 23:59:59 GMT") 

f495df43e1ce0b277312fcd90aaeb06cf26cf2c3
Louis Bodnar <pezmanlou@gmail.com>
2012-02-21 14:49:59 -0500

    miniapp works!!

--- miniapp.py
+++ miniapp.py
@@ -1,35 +1,102 @@
 from meep_example_app import MeepExampleApp, initialize
-import os
+import urllib
+import os.path
 import sys
 import datetime
 
-def main():
-    app = MeepExampleApp()
-    environ = {}
+global _status
+global _headers
+# the function we will use to pass to __call__
+def fake_start_response(status, headers):
+    global _status
+    global _headers
+    _status = status
+    _headers = headers
 
-    def fake_start_response(status, headers):
-        print status
-        print headers
-        print datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
-        print '\n'
+argList = []
 
-    for arg in sys.argv: # for eery request
-        if (arg == sys.argv[0]):
-            continue
-        f = open(os.getcwd() + "/requests/" + arg)
-        for line in f.readlines(): # for eery line in the request
-            if line.startswith('GET'):
-                line = line.rstrip('\n')
-                words = line.split(' ')
-                environ['REQUEST_METHOD'] = 'GET'
-                environ['PATH_INFO'] = words[1]
-                environ['SERVER_PROTOCOL'] = words[2]
-            elif line.startswith('cookie:'):
-                line = line.rstrip('\n')
-                line = line.lstrip('cookie: ')
-                environ['HTTP_COOKIE'] = line
+for arg in sys.argv:
+    argList.append(arg)
 
-        app.__call__(environ, fake_start_response) # respond
+# remove script path
+argList = argList[1:]
 
+if argList:
+    filepath = argList.pop(0)
+    filepath = os.path.abspath(filepath)
+else:
+    filepath = raw_input("Specify a filepath, can be absolute or relative: ")
+    filepath = os.path.abspath(filepath)
 
-main()
+try:
+    fh = open(filepath, 'r')
+except:
+    print filepath,
+    print "is not readable or does not exist. Exiting."
+    sys.exit()
+
+allLines = []
+for line in fh.readlines():
+    allLines.append(line.strip())
+fh.close()
+
+# read request line
+request = allLines.pop(0)
+# split on whitespace
+requestInfoList = request.split(' ')
+#print requestInfoList
+
+fullQueryList = requestInfoList[1].split('?')
+#print fullQueryList
+
+form_dict = {}
+
+# no variables to get
+if len(fullQueryList) == 1:
+    path_info = fullQueryList[0]
+# we need to do further processing on the variables
+else:
+    path_info = fullQueryList[0]
+    tmpVariables = fullQueryList[1].split('&')
+    for variablePair in tmpVariables:
+        key,value = variablePair.split('=')
+        form_dict[key] = urllib.unquote_plus(value)
+
+#print allLines
+
+cookie = ''
+# get the cookie
+for line in allLines:
+    tmp = line.split(':', 2)
+    if tmp[0] == 'cookie':
+        cookie = tmp[1].strip()
+
+#print 'cookie = ', cookie
+
+environ = {} # make a fake request dictionary
+environ['PATH_INFO'] = path_info
+environ['wsgi.input'] = ''
+environ['HTTP_COOKIE'] = cookie
+if len(form_dict):
+    environ['QUERY_STRING'] = urllib.urlencode(form_dict)
+
+initialize()
+app = MeepExampleApp()
+
+#print environ
+data = app(environ, fake_start_response)
+
+#print _status
+#print _headers
+
+#print data
+
+print "HTTP/1.0", _status
+now = datetime.datetime.now()
+print "Date:", now.strftime("%a, %d %b %Y %H:%M:%S EST")
+print "Server: WSGIServer/0.1 Python/2.5"
+for x in _headers:
+    print x
+print
+for x in data:
+    print x

6f67b9d1f01712c3315ead126ade8ddd4235c255
unknown <Kevin@Kevin-PC.(none)>
2012-02-21 12:06:27 -0500

    added more tests

--- test_app.py
+++ test_app.py
@@ -2,59 +2,75 @@ import unittest
 import meep_example_app
 import meeplib
 class TestApp(unittest.TestCase):
-    def setUp(self):
-        meep_example_app.initialize()
-        app = meep_example_app.MeepExampleApp()
-        self.app = app
+   def setUp(self):
+      meep_example_app.initialize()
+      app = meep_example_app.MeepExampleApp()
+      self.app = app
 
-    def test_index(self):
-        environ = {} # make a fake dict
-        environ['PATH_INFO'] = '/'
+   def test_index(self):
+      environ = {} # make a fake dict
+      environ['PATH_INFO'] = '/'
+      def fake_start_response(status, headers):
+         assert status == '200 OK'
+         assert ('Content-type', 'text/html') in headers
 
-        def fake_start_response(status, headers):
-            assert status == '200 OK'
-            assert ('Content-type', 'text/html') in headers
+      data = self.app(environ, fake_start_response)
+      assert 'Add a message' in data[0]
+      assert 'Show messages' in data[0]
 
-        data = self.app(environ, fake_start_response)
-        assert 'Add a message' in data[0]
-        assert 'Show messages' in data[0]
-
-    def test_list_messages(self):
-        environ = {} # make a fake dict
-        environ['PATH_INFO'] = '/m/list'
-
-        def fake_start_response(status, headers):
-            assert status == '200 OK'
-            assert ('Content-type', 'text/html') in headers
-        data = self.app(environ, fake_start_response)
-
-        assert 'id:' in data[0]
-        assert 'title:' in data[0]
-        assert 'author:' in data[0]
-        assert 'message:' in data[0]
-        assert 'Reply' in data[0]
-        assert 'Delete Message' in data[0]
-        assert 'Search' in data[0]
+   def test_list_messages(self):
+      environ = {} # make a fake dict
+      environ['PATH_INFO'] = '/m/list'
+      def fake_start_response(status, headers):
+         assert status == '200 OK'
+         assert ('Content-type', 'text/html') in headers
+      data = self.app(environ, fake_start_response)
+      assert 'id:' in data[0]
+      assert 'title:' in data[0]
+      assert 'author:' in data[0]
+      assert 'message:' in data[0]
+      assert 'Reply' in data[0]
+      assert 'Delete Message' in data[0]
+      assert 'Search' in data[0]
         
-    def test_delete(self):
-        u = meeplib.User('foo', 'bar')
-        m = meeplib.Message('my title', 'This is my message!', u)
+   def test_delete(self):
+      u = meeplib.User('foo', 'bar')
+      m = meeplib.Message('my title', 'This is my message!', u)
 
-        assert m in meeplib._messages.values()
+      assert m in meeplib._messages.values()
+      meeplib.delete_message(m)
 
-        meeplib.delete_message(m)
-
-        assert m not in meeplib._messages.values()
+      assert m not in meeplib._messages.values()
 
 
-    def test_add(self):
-        u = meeplib.User('bar', 'foo')
-        m = meeplib.Message('The Title', 'Content', u)
+   def test_add(self):
+      u = meeplib.User('bar', 'foo')
+      m = meeplib.Message('The Title', 'Content', u)
 
-        assert m in meeplib._messages.values()
+      assert m in meeplib._messages.values()
 
-    def tearDown(self):
-        pass
+   def test_login(self):
+      environ = {} # make a fake dict
+      environ['PATH_INFO'] = '/'
+      def fake_start_response(status, headers):
+         assert status == '200 OK'
+         assert ('Content-type', 'text/html') in headers
+
+      environ["HTTP_COOKIE"] = 'username=test'
+      data = self.app(environ, fake_start_response)
+      assert 'you are logged in' in data[0]
+		  
+   def test_logout(self):
+      environ = {} # make a fake dict
+      environ['PATH_INFO'] = '/'
+      def fake_start_response(status, headers):
+         assert status == '200 OK'
+         assert ('Content-type', 'text/html') in headers
+      environ["HTTP_COOKIE"] = 'username='
+      data = self.app(environ, fake_start_response)
+      assert 'You are not logged in' in data[0]		  
+   def tearDown(self):
+      pass
 
 if __name__ == '__main__':
-    unittest.main()
+	unittest.main()
